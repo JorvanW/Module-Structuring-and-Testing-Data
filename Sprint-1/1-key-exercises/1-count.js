3aae31c1ad41de812af555ace83c5772e5c6f969
Jorvan White <jorvanwhite@gmail.com>
2026-08-03 17:29:09 +0100

    Clean up comments in 1-count.js
    Removed comments explaining variable assignment and declaration.

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,7 +4,3 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
-
-// = is assigning the value of 'count', 
-// 'let' is a declararation than enables its subject (count) to be changed
-// line 3 is taking the current value in line 1 (0) and by using '=' it is reassigning the value of 'count' to equal itself plus 1
